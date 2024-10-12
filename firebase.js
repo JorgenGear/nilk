971dc1b668993a3880c59644a0f8c9368c6d0338
JorgenGear <rhettjorg@gmail.com>
2024-10-12 16:20:59 -0600

    initial deployment

--- firebase.js
+++ firebase.js
@@ -4,13 +4,7 @@ import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/aut
 import {AsyncStorage} from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_hKoSpfCnRiTbBhX16xqL4Uyad0UNzpM",
-    authDomain: "nilk-3a766.firebaseapp.com",
-    projectId: "nilk-3a766",
-    storageBucket: "nilk-3a766.appspot.com",
-    messagingSenderId: "592973766791",
-    appId: "1:592973766791:web:b8fe4248d962116f1f5822",
-    measurementId: "G-PCQQZH0F0R"
+
 };
 
 const app = initializeApp(firebaseConfig);
